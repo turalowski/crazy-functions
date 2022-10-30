b4f3f89f26a1042194863cde0f738d862edcfc5e
Tural Hajiyev <thajiyev9@outlook.com>
2022-10-30 11:39:19 +0100

    Function to get last item of array

---
+++ __tests__/last-item-of-array.test.js
@@ -0,0 +1,9 @@
+import { lastItemOfArray } from '../src/last-item-of-array';
+
+describe("Let's make working with arrays easier", () => {
+  test('It should find last item of array', () => {
+    const items = [1, 2, 3, 4, 5];
+    const lastItem = lastItemOfArray(items);
+    expect(lastItem).toEqual(5);
+  });
+});
---
+++ src/index.js
@@ -0,0 +1,2 @@
+// It returns last element of passed array.
+export { lastItemOfArray } from './last-item-of-array';
