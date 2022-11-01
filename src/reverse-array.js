47439dd4f4fc1a5ae12d5a3f45bb4e12c7d720e9
Tural Hajiyev <hcyv19@gmail.com>
2022-11-01 18:00:42 +0100

    Add function to get reversed versino of passed array

---
+++ src/reverse-array.js
@@ -0,0 +1,10 @@
+/**
+ * Represents a function which returns first reversed version of array.
+ * @function
+ * @param {Array} array - List of elements
+ * @returns {any} - Reversed array
+ */
+ export function reverseArray(array) {
+    return array.reverse();
+}
+  
