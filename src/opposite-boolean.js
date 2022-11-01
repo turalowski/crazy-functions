00c94dbe1d10becdf80d907baba797557ed92b18
Tural Hajiyev <hcyv19@gmail.com>
2022-11-01 19:24:35 +0100

    Opposite value of boolean argument

---
+++ src/opposite-boolean.js
@@ -0,0 +1,10 @@
+/**
+ * Represents a function which returns opposite value of boolean argument.
+ * @function
+ * @param {Boolean} value - Passed boolean argument
+ * @returns {Boolean} - Opposite value of passed argument
+ */
+ export function oppositeBoolean(value) {
+    return !value;
+  }
+  
